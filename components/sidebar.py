019efe3d23db500b190fac6973351f88eda03918
monika-sahay <monika_sahay@yahoo.com>
2023-11-20 10:29:07 +0100

    added module docstrings

--- components/sidebar.py
+++ components/sidebar.py
@@ -1,3 +1,7 @@
+'''
+This module encapsulates the complexity of creating a Sidebar 
+and promotes a clean and readable interface for creating instances
+'''
 from frap.components.sidebarcomponents import SidebarStyle, SidebarComponents
 
 
